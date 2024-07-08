34975eda84861bfa0f6138747cbea6a9f065c211
the-de <the_desire_of_my_heart@hotmail.com>
2024-07-08 23:24:32 +0900

    libフォルダ修正

--- rust_c/build.rs
+++ rust_c/build.rs
@@ -2,6 +2,7 @@ use std::env;
 
 fn main(){
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!(r"cargo:rustc-link-search=native={}/../sampl_dll/x64/Debug",dir);
+    let deb_rel = env::var("PROFILE").unwrap();    
+    println!(r"cargo:rustc-link-search=native={}/target/{}",dir,deb_rel);
     println!("cargo:rustc-link-lib=dylib={}","sampl_dll");
 }
